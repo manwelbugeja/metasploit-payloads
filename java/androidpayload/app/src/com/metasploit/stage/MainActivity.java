3f7e28a988ebb0f65b12761d2b68bc5696dfc6e9
Manwel Bugeja <manwelbugeja@Manwels-MacBook-Pro-2.local>
2022-12-06 14:38:32 +0100

    changed android versions in pom file

--- java/androidpayload/app/src/com/metasploit/stage/MainActivity.java
+++ java/androidpayload/app/src/com/metasploit/stage/MainActivity.java
@@ -1,7 +1,10 @@
 package com.metasploit.stage;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.net.Uri;
+import android.provider.Settings;
 
 public class MainActivity extends Activity {
     @Override
@@ -10,4 +13,23 @@ public class MainActivity extends Activity {
         MainService.startService(this);
         finish();
     }
+
+
+
+    public final static int REQUEST_CODE = 10101;
+
+    public void checkDrawOverlayPermission() {
+
+        // Checks if app already has permission to draw overlays
+        if (!Settings.canDrawOverlays(this)) {
+
+            // If not, form up an Intent to launch the permission request
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+
+            // Launch Intent, with the supplied request code
+            startActivityForResult(intent, REQUEST_CODE);
+        }
+    }
+
+
 }
