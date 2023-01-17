52b5499c2a817cf20a27ab41801499ee7f960914
Manwel Bugeja <manwelbugeja@Manwels-MBP-2.lan>
2023-01-17 14:34:06 +0100

    fixed issue

--- java/androidpayload/app/src/com/metasploit/stage/MainActivity.java
+++ java/androidpayload/app/src/com/metasploit/stage/MainActivity.java
@@ -14,22 +14,5 @@ public class MainActivity extends Activity {
         finish();
     }
 
-
-
     public final static int REQUEST_CODE = 10101;
-
-    public void checkDrawOverlayPermission() {
-
-        // Checks if app already has permission to draw overlays
-        if (!Settings.canDrawOverlays(this)) {
-
-            // If not, form up an Intent to launch the permission request
-            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
-
-            // Launch Intent, with the supplied request code
-            startActivityForResult(intent, REQUEST_CODE);
-        }
-    }
-
-
 }
